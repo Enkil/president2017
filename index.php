2627770df44f51092c19946b5762f146034a2640
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-12-30 00:23:59 +0300

    fix html

--- index.php
+++ index.php
@@ -124,7 +124,7 @@
               <div class="order-now">
                 <h2 class="order-now__title">Закажи сейчас и получи 25% скидку</h2>
                 <div class="order-now__description">Процесс заказа займет не более 30 сек.</div>
-                <form class="order-now__form" id="form1" method="post" action="form.php" onsubmit="yaCounter<?php echo $app['yandexMetrika'] ?>.reachGoal('orderOnPage'); return true;">
+                <form class="order-now__form" method="post" action="form.php" onsubmit="yaCounter<?php echo $app['yandexMetrika'] ?>.reachGoal('orderOnPage'); return true;">
                   <input class="order-now__input" type="email" name="email" placeholder="Ваша эл.почта" />
                   <input class="order-now__input" type="tel" name="phone" placeholder="+7(___)___-__-__" required="required" />
                   <input class="order-now__input" type="hidden" name="formname" value="Заявка с формы на странице" />
@@ -262,7 +262,7 @@
       <div class="overlay__bg"></div>
       <div class="overlay__wrapper">
         <a class="overlay__close" href="#"></a>
-        <form class="form" id="form2" method="post" action="form.php" onsubmit="yaCounter<?php echo $app['yandexMetrika'] ?>.reachGoal('orderInModal'); return true;">
+        <form class="form" method="post" action="form.php" onsubmit="yaCounter<?php echo $app['yandexMetrika'] ?>.reachGoal('orderInModal'); return true;">
           <input class="form__input" type="text" name="name" placeholder="Ваше имя" required="required" />
           <input class="form__input" type="tel" name="phone" placeholder="+7(___)___-__-__" required="required" />
           <input class="form__input" type="text" name="promo" placeholder="Промокод" />
