08ed451881fc0a903eae62a80a48fa873d440afb
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-12-30 04:43:02 +0300

    form

--- form.php
+++ form.php
@@ -11,11 +11,11 @@ if(isset($_POST['pageFormType']) or isset($_POST['modalFormType']))
 
     // Forms data
     
-//    if (isset($_POST['pageFormType'])) {
-//        $formname = htmlspecialchars(isset($_POST['pageFormType']));
-//    } else {
-//        $formname = htmlspecialchars(isset($_POST['modalFormType']));
-//    }
+    if ($_POST['formname'] == 'pageForm') {
+        $formname = 'Заявка с формы на странице';
+    } elseif($_POST['formname'] == 'modalForm') {
+        $formname = 'Заявка с формы в модальном окне';
+    }
     
 //    $formname = htmlspecialchars(isset($_POST['formname']) ? $_POST['formname'] : '');
     $email = htmlspecialchars(isset($_POST['email']) ? $_POST['email'] : '');
