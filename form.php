c63137109aea3be5c3acafebf9a40439ed6a0016
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-12-31 15:22:02 +0300

    gsheets

--- form.php
+++ form.php
@@ -15,52 +15,51 @@ echo var_dump($_POST);
  *
  */
 // These settings are found on google developer console
-//$CLIENT_APP_NAME = $settings['googleSpreedSheetsAppName'];
-//$CLIENT_ID       = $settings['googleSpreedSheetsClientId'];
-//$CLIENT_EMAIL    = $settings['googleSpreedSheetsClientEmail'];
-//$CLIENT_KEY_PATH = $settings['googleSpreedSheetsP12'];
-//$CLIENT_KEY_PW   = 'notasecret';
-//
-//$objClientAuth  = new Google_Client ();
-//$objClientAuth->setClassConfig('Google_Cache_File', 'directory', 'tmp/Google_Client');
-//$objClientAuth -> setApplicationName ($CLIENT_APP_NAME);
-//$objClientAuth -> setClientId ($CLIENT_ID);
-//$objClientAuth -> setAssertionCredentials (new Google_Auth_AssertionCredentials (
-//    $CLIENT_EMAIL,
-//    array('https://spreadsheets.google.com/feeds','https://docs.google.com/feeds'),
-//    file_get_contents ($CLIENT_KEY_PATH),
-//    $CLIENT_KEY_PW
-//));
-//$objClientAuth->getAuth()->refreshTokenWithAssertion();
-//$objToken  = json_decode($objClientAuth->getAccessToken());
-//$accessToken = $objToken->access_token;
-//
-//
+$CLIENT_APP_NAME = $settings['googleSpreedSheetsAppName'];
+$CLIENT_ID       = $settings['googleSpreedSheetsClientId'];
+$CLIENT_EMAIL    = $settings['googleSpreedSheetsClientEmail'];
+$CLIENT_KEY_PATH = $settings['googleSpreedSheetsP12'];
+$CLIENT_KEY_PW   = 'notasecret';
+
+$objClientAuth  = new Google_Client ();
+$objClientAuth->setClassConfig('Google_Cache_File', 'directory', 'tmp/Google_Client');
+$objClientAuth -> setApplicationName ($CLIENT_APP_NAME);
+$objClientAuth -> setClientId ($CLIENT_ID);
+$objClientAuth -> setAssertionCredentials (new Google_Auth_AssertionCredentials (
+    $CLIENT_EMAIL,
+    array('https://spreadsheets.google.com/feeds','https://docs.google.com/feeds'),
+    file_get_contents ($CLIENT_KEY_PATH),
+    $CLIENT_KEY_PW
+));
+$objClientAuth->getAuth()->refreshTokenWithAssertion();
+$objToken  = json_decode($objClientAuth->getAccessToken());
+$accessToken = $objToken->access_token;
+
+
 ///**
 // * Initialize the service request factory
 // */
-//use Google\Spreadsheet\DefaultServiceRequest;
-//use Google\Spreadsheet\ServiceRequestFactory;
-//
-//$serviceRequest = new DefaultServiceRequest($accessToken);
-//ServiceRequestFactory::setInstance($serviceRequest);
-//
-//
+use Google\Spreadsheet\DefaultServiceRequest;
+use Google\Spreadsheet\ServiceRequestFactory;
+
+$serviceRequest = new DefaultServiceRequest($accessToken);
+ServiceRequestFactory::setInstance($serviceRequest);
+
 ///**
-// * Get spreadsheet by title
+// * Get spreadsheet by id
 // */
-//$spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
-//$spreadsheetFeed = $spreadsheetService->getSpreadsheets();
-//$spreadsheet = $spreadsheetFeed->getById($settings['googleSpreedSheetsSpreedSheetID']);
-//
-//
-//
-//
+$spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
+$spreadsheetFeed = $spreadsheetService->getSpreadsheets();
+$spreadsheet = $spreadsheetFeed->getById($settings['googleSpreedSheetsSpreedSheetID']);
+
+
+
+
 ///**
 // * Get particular worksheet of the selected spreadsheet
 // */
-//$worksheetFeed = $spreadsheet->getWorksheets();
-//$worksheet = $worksheetFeed->getById($settings['googleSpreedSheetsWorkSheetID']);
+$worksheetFeed = $spreadsheet->getWorksheets();
+$worksheet = $worksheetFeed->getById($settings['googleSpreedSheetsWorkSheetID']);
 
 
 
@@ -171,7 +170,7 @@ if(isset($_POST['formname']))
      * Insert row entries
      * Supposing, that there are two headers 'name' and 'age'
      */
-//    $row = array('name'=>'John', 'age'=>25);
-//    $listFeed->insert($row);
+    $row = array('name'=>'John', 'age'=>25);
+    $listFeed->insert($row);
 
 }
