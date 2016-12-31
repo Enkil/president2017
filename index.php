28ee891299bdb65a183df7e8b502934e33940c60
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-12-31 16:16:02 +0300

    gsheets

--- index.php
+++ index.php
@@ -128,7 +128,7 @@
                   <input class="order-now__input" type="tel" name="phone" placeholder="Ваш телефон" required="required" />
                   <input class="order-now__input" type="hidden" name="formname" value="Заявка с формы на странице"/>
                   <button class="btn btn--form" type="submit">Заказать</button>
-                  <span>Спасибо, заявка принята! Наши сотрудники свяжутся с Вами в ближайшее время!</span>
+                  <span class="order-now__success">Спасибо, заявка принята! Наши сотрудники свяжутся с Вами в ближайшее время!</span>
                 </form>
               </div>
             </div>
@@ -268,6 +268,7 @@
           <input class="form__input" type="text" name="promo" placeholder="Промокод" />
           <input class="form__input" type="hidden" name="formname" value="Заявка с формы в модальном окне"/>
           <button class="btn btn--form" type="submit">Заказать</button>
+            <span class="form__success">Спасибо, заявка принята! Наши сотрудники свяжутся с Вами в ближайшее время!</span>
         </form>
       </div>
     </div>
