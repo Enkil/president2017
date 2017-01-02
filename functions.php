9e60f8e40fa29b047a280ab14bf293f954a29f6e
Igor [Enkil] Timohin <timohin.i@gmail.com>
2017-01-02 13:21:45 +0300

    refcode

--- functions.php
+++ functions.php
@@ -91,7 +91,7 @@ function sendSms($settings,$data){
 
 function insertRowInGoogleSpreadSheet($listFeed,$today,$data,$country,$city,$region,$utm){
     $refPattern = '/^president2017-/';
-    $refCode = preg_match($refPattern,$utm['utm_campaign'],$arr);
-    $row = array('дата'=>$today, 'статус'=>'Новая с телефоном', 'телефон'=>$data['phone'], 'e-mail'=>$data['email'], 'фио'=>$data['name'],  'адрес'=>adress($country,$city,$region),'промо-код'=>$data['promo'],'источник'=>($utm['utm_source']) ? $utm['utm_source'] : 0,'рефкод'=> $arr[1]);
+    $refCode = preg_match($refPattern,$utm['utm_campaign'],$matches);
+    $row = array('дата'=>$today, 'статус'=>'Новая с телефоном', 'телефон'=>$data['phone'], 'e-mail'=>$data['email'], 'фио'=>$data['name'],  'адрес'=>adress($country,$city,$region),'промо-код'=>$data['promo'],'источник'=>($utm['utm_source']) ? $utm['utm_source'] : 0,'рефкод'=> $matches[0]);
     $listFeed->insert($row);
 }
