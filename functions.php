f1741640b14a6d5121cc9f12480e5996cfc66007
Igor [Enkil] Timohin <timohin.i@gmail.com>
2017-01-02 12:54:32 +0300

    refcode

--- functions.php
+++ functions.php
@@ -90,6 +90,8 @@ function sendSms($settings,$data){
 }
 
 function insertRowInGoogleSpreadSheet($listFeed,$today,$data,$country,$city,$region,$utm){
-    $row = array('дата'=>$today, 'статус'=>'Новая с телефоном', 'телефон'=>$data['phone'], 'e-mail'=>$data['email'], 'фио'=>$data['name'],  'адрес'=>adress($country,$city,$region),'промо-код'=>$data['promo'],'источник'=>($utm['utm_source']) ? $utm['utm_source'] : ' ');
+    $refPattern = '^president2017-';
+    $refCode = preg_match($refPattern,$utm['utm_compaign'],$arr);
+    $row = array('дата'=>$today, 'статус'=>'Новая с телефоном', 'телефон'=>$data['phone'], 'e-mail'=>$data['email'], 'фио'=>$data['name'],  'адрес'=>adress($country,$city,$region),'промо-код'=>$data['promo'],'источник'=>($utm['utm_source']) ? $utm['utm_source'] : 0,'рефкод'=> $arr[0]);
     $listFeed->insert($row);
 }
